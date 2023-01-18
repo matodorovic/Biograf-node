91205eee887b801636712751e815d667c6c4ba5f
Marta Anna Todorovic <martaannatodorovic@gmail.com>
2023-01-18 20:21:38 +0100

    Cloned uppgift2-biograf

---
+++ src/JS/main.js
@@ -0,0 +1,11 @@
+import { mobileMenu } from "./mobilmenu.js";
+import listenerMenuBtn from "./listeners.js";
+import  sidebar  from "./sidebar.js";
+import  checkLocationForStyle  from "./checkLocation.js";
+import  moviesOnStartpage  from "./moviesOnStartpage.js";
+
+mobileMenu();
+moviesOnStartpage();
+sidebar();
+checkLocationForStyle();
+listenerMenuBtn();
